a6b1eab56290da45efd61dac719d9f75cd78b07e
喷火的神灵 <141718818+ZhaoZan010903@users.noreply.github.com>
2023-08-15 16:33:51 +0800

    DAY02 -- list类型,测试代码

--- note/Redis数据类型.md
+++ note/Redis数据类型.md
@@ -272,3 +272,52 @@ LRANGE key start stop -- 返回列表key中指定区间内的元素,区间以偏
 
 **_应用场景:_**Redis list的应用场景非常多,也是Redis最重要的数据结构之一,比如微博的关注列表,粉丝列表、热搜、top榜单等都可以用Redis的List结构来实现
 
+```java
+@SpringBootTest
+public class RedisList {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+    
+    //插入
+    @Test
+    public void ListTest(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        for (int i = 0; i<10;i++){
+            // 往尾部插入
+            list.rightPush(i);
+            // 往头部插入
+            list.leftPush(i);
+            //往尾部批量插入
+            list.rightPushAll(1,2,3,4,5);
+            //往尾部批量插入
+            list.leftPushAll(1,2,3,4,5);
+        }
+    }
+
+    //查询
+    @Test
+    public void ListTest2(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        //范围获取
+        System.out.println(list.range(0, list.size()));
+        //根据下表获取某一个
+        System.out.println(list.index(5));
+    }
+
+    //删除
+    @Test
+    public void ListTest3(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        // 从头部删除  参数为删几个
+        list.leftPop();
+        // 从尾部删除指定数量
+        list.rightPop();
+        // 根据值进行删除
+        list.remove(1,0);
+        // 保留指定范围 ,其余都删掉
+//        list.trim(1,2);
+        list.set(1,10);
+    }
+}
+```
---
+++ src/test/java/cn/note/redis/RedisTest/RedisList.java
@@ -0,0 +1,56 @@
+package cn.note.redis.RedisTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+public class RedisList {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+
+    //插入
+    @Test
+    public void ListTest(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        for (int i = 0; i<10;i++){
+            // 往尾部插入
+            list.rightPush(i);
+            // 往头部插入
+            list.leftPush(i);
+            //往尾部批量插入
+            list.rightPushAll(1,2,3,4,5);
+            //往尾部批量插入
+            list.leftPushAll(1,2,3,4,5);
+        }
+    }
+
+    //查询
+    @Test
+    public void ListTest2(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        //范围获取
+        System.out.println(list.range(0, list.size()));
+        //根据下表获取某一个
+        System.out.println(list.index(5));
+    }
+
+    //删除
+    @Test
+    public void ListTest3(){
+        BoundListOperations list = redisTemplate.boundListOps("list");
+        // 从头部删除  参数为删几个
+        list.leftPop();
+        // 从尾部删除指定数量
+        list.rightPop();
+        // 根据值进行删除
+        list.remove(1,0);
+        // 保留指定范围 ,其余都删掉
+//        list.trim(1,2);
+        list.set(1,10);
+    }
+}
