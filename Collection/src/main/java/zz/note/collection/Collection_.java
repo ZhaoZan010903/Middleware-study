a19f83933e9ef8692729016e1b1429864833afcb
喷火的神灵 <141718818+ZhaoZan010903@users.noreply.github.com>
2023-08-18 19:59:46 +0800

    DAY05 -- 集合 笔记

---
+++ Collection/src/main/java/zz/note/collection/Collection_.java
@@ -0,0 +1,12 @@
+package zz.note.collection;
+
+import java.util.Collection;
+import java.util.Map;
+
+public class Collection_ {
+    public static void main(String[] args) {
+//        Collection
+//        Map
+    }
+
+}
