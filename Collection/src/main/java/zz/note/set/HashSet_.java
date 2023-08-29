bbde87c59f2c1a59debd55b8447573e5a85e5a35
喷火的神灵 <141718818+ZhaoZan010903@users.noreply.github.com>
2023-08-30 05:31:42 +0800

    DAY05 -- Set -- HashSet底层

--- Collection/src/main/java/zz/note/set/HashSet_.java
+++ Collection/src/main/java/zz/note/set/HashSet_.java
@@ -1,9 +1,15 @@
 package zz.note.set;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class HashSet_ {
     public static void main(String[] args) {
-        var hashSet = new HashSet();
+        Set set = new HashSet();
+        set.add("Java");
+        set.add("Python");
+        set.add("Go");
+        set.add("Java");
+        System.out.println(set);
     }
 }
