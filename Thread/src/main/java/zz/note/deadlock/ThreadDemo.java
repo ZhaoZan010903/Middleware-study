7821f93ec74730ffccc092491e82409423951d89
喷火的神灵 <zhaozan010903@yahoo.com>
2023-09-09 16:00:10 +0800

    DAY07 -- 线程 -- 锁

--- Thread/src/main/java/zz/note/deadlock/ThreadDemo.java
+++ Thread/src/main/java/zz/note/deadlock/ThreadDemo.java
@@ -1,18 +1,17 @@
-package zz.note.threadsafe3;
+package zz.note.deadlock;
 
+import zz.note.threadsafe1.MyThread;
 
 public class ThreadDemo {
     public static void main(String[] args) {
         MyThread t1 = new MyThread();
         MyThread t2 = new MyThread();
-        MyThread t3 = new MyThread();
 
-        t1.setName("第一个");
-        t2.setName("第二个");
-        t3.setName("第三个");
+        t1.setName("线程A");
+        t2.setName("线程B");
 
         t1.start();
         t2.start();
-        t3.start();
+
     }
 }
